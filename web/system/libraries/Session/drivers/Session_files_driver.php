4adb88fd3337a9923505c63db4abe7ba19f8b5f8
Le Anh Duc <ducla@lifull-tech.vn>
2018-02-06 14:02:59 +0700

    change env

--- web/system/libraries/Session/drivers/Session_files_driver.php
+++ web/system/libraries/Session/drivers/Session_files_driver.php
@@ -135,6 +135,7 @@ class CI_Session_files_driver extends CI_Session_driver implements SessionHandle
 		{
 			if ( ! mkdir($save_path, 0700, TRUE))
 			{
+				var_dump($save_path);
 				throw new Exception("Session: Configured save path '".$this->_config['save_path']."' is not a directory, doesn't exist or cannot be created.");
 			}
 		}
