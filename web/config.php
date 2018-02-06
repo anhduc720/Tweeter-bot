93e31628e4b57a06c8f16a5c076669ad65edc6c5
Le Anh Duc <ducla@lifull-tech.vn>
2018-02-06 13:46:56 +0700

    change env

--- web/config.php
+++ web/config.php
@@ -7,10 +7,8 @@ define("DB_PASSWORD", "bbd82701");
 define("DB_NAME", "heroku_7ce41d5935040b6");
 
 /*TWITTER APP SETTINGS*/
-// create at https://apps.twitter.com
-//Name app: twapps234 account owner: Rocio Shepherd
 define('CONSUMER_KEY', 'fl8RPeCd4vrrAiZ8HJ3U7OiJE');
 define('CONSUMER_KEY_SECRET', 'fzFVWeM1EoQDb0YEHiy1JZMGq9nvRPW2YqliWK3ImYuEGbrGLk');
-define('OAUTH_CALLBACK', 'https://twiterbot.herokuapp.com/callback);
+define('OAUTH_CALLBACK', 'https://twiterbot.herokuapp.com/callback');
 
 ?>
