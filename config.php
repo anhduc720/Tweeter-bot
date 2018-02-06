c4ecc85a4afdd38994dc5029b65877dc6a628610
Le Anh Duc <ducla@lifull-tech.vn>
2018-02-06 12:55:25 +0700

    add db config

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
 
 /* DATABASE SETTINGS */
-define("DB_HOSTNAME", "localhost");
-define("DB_USERNAME", "");
-define("DB_PASSWORD", "");
-define("DB_NAME", "");
+define("DB_HOSTNAME", "us-cdbr-iron-east-05.cleardb.net/heroku_7ce41d5935");
+define("DB_USERNAME", "b639666e16822a");
+define("DB_PASSWORD", "bbd82701");
+define("DB_NAME", "heroku_7ce41d5935040b6");
 
 /*TWITTER APP SETTINGS*/
 // create at https://apps.twitter.com
 //Name app: twapps234 account owner: Rocio Shepherd
-define('CONSUMER_KEY', '');
-define('CONSUMER_KEY_SECRET', '');
-define('OAUTH_CALLBACK', 'http://your-instalation.com/tweendo/index.php/callback');
+define('CONSUMER_KEY', 'fl8RPeCd4vrrAiZ8HJ3U7OiJE');
+define('CONSUMER_KEY_SECRET', 'fzFVWeM1EoQDb0YEHiy1JZMGq9nvRPW2YqliWK3ImYuEGbrGLk');
+define('OAUTH_CALLBACK', 'https://twiterbot.herokuapp.com/callback);
 
 ?>
